922a33f9de454b54ce8cbe9e12a82c103130e8d5
priyadarshu <priyadarshinichettiar@gmail.com>
2021-01-04 17:10:22 +0530

    Added  home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sample_ui_2/screens/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +8,27 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "My Sample UI 2",
+      theme: ThemeData(
+        primaryColor: Colors.blueGrey[300],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ShopZone'),
+          centerTitle: true,
+          actions: [
+            Icon(Icons.search),
+            Icon(Icons.shopping_cart),
+          ],
+        ),
+        drawer: Drawer(),
+        body: MyHome(),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+      ),
+    );
   }
 }
